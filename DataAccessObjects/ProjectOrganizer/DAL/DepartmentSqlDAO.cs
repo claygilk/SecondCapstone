c2d228f5a6c03cb09cd940106fb819d67640abbb
claygilk <46429931+claygilk@users.noreply.github.com>
2021-06-14 21:07:00 -0400

    Employee/Dept DAO

--- DataAccessObjects/ProjectOrganizer/DAL/DepartmentSqlDAO.cs
+++ DataAccessObjects/ProjectOrganizer/DAL/DepartmentSqlDAO.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectOrganizer.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace ProjectOrganizer.DAL
 {
@@ -9,8 +10,8 @@ namespace ProjectOrganizer.DAL
         private readonly string connectionString;
 
         private const string SqlGetDepartment = "Select * FROM department;";
-        private const string SqlCreateDepartment = "Insert Into department (name) VALUES @department_name;";
-        private const string SqlUpdateDepartment = "Update department set (name) = @newName where name = @oldName;";
+        private const string SqlCreateDepartment = "INSERT INTO department (name) VALUES (@department_name); SELECT @@IDENTITY;";
+        private const string SqlUpdateDepartment = "Update department set name = @newName where department_id = @departmentId;";
 
         // Single Parameter Constructor
         public DepartmentSqlDAO(string dbConnectionString)
@@ -24,7 +25,36 @@ namespace ProjectOrganizer.DAL
         /// <returns></returns>
         public ICollection<Department> GetDepartments()
         {
-            throw new NotImplementedException();
+            List<Department> departments = new List<Department>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlGetDepartment, conn);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Department dept = new Department();
+
+                        dept.Id = Convert.ToInt32(reader["department_id"]);
+                        dept.Name = Convert.ToString(reader["name"]);
+
+                        departments.Add(dept);
+
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                Console.WriteLine("No departments found");
+            }
+
+            return departments;
         }
 
         /// <summary>
@@ -34,7 +64,26 @@ namespace ProjectOrganizer.DAL
         /// <returns>The id of the new department (if successful).</returns>
         public int CreateDepartment(Department newDepartment)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlCreateDepartment, conn);
+
+                    command.Parameters.AddWithValue("@department_name", newDepartment.Name);
+
+                    newDepartment.Id = Convert.ToInt32(command.ExecuteScalar());
+
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Couldn't create department");
+            }
+            
+            return newDepartment.Id;
         }
 
         /// <summary>
@@ -44,7 +93,28 @@ namespace ProjectOrganizer.DAL
         /// <returns>True, if successful.</returns>
         public bool UpdateDepartment(Department updatedDepartment)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlUpdateDepartment, conn);
+
+                    command.Parameters.AddWithValue("@newName", updatedDepartment.Name);
+                    command.Parameters.AddWithValue("@departmentId", updatedDepartment.Id);
+
+                    command.ExecuteNonQuery();
+                }
+
+                return true;
+            }
+            catch (SqlException)
+            {
+                Console.WriteLine("Could not update department");
+            }
+
+            return false;
         }
 
     }
